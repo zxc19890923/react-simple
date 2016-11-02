464c33804e77145107db2db910d03941a038744b
zhangxuchao <2212362272@qq.com>
2016-11-02 13:42:31 +0800

    Add files via upload

---
+++ about.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import $ from "jquery";
+// /about
+var About = React.createClass({
+    render() {
+        return (
+            <div>about</div>
+        )
+    }
+});
+export default About;
